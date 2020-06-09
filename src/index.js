6b81b686d380150fc6696028a2397900671af382
yazen firas <psyazen3@gmail.com>
2020-06-09 12:58:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  
+  fetch(imgUrl).then(res=>{return res.json()})
+  .then(json=>
+   {
+  let all=json.message
+  for (let i of all)
+  {
+   let img = document.getElementById("img").src=i
+    
+  }
+      
+  })
+  
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  
+  fetch(breedUrl).then(res=>{
+    return res.json()
+  })
+  
+  .then(json=>{
+    
+    let all2=json.message
+    for(let f in all2)
+    
+    {
+    console.log(json.message)
+   
+  }
+  
+   })
+  
+  
+  
+})
